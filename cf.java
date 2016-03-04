8e6e2c25dc2b3cb1ae29be663deeb320c7e2e945
ravii0020 <raviinightei@outlook.com>
2016-03-04 15:24:14 +0700

    new

--- cf.java
+++ cf.java
@@ -1,6 +1,24 @@
 class cf {
 	public static void main(String[] arg) {
 		System.out.println("test");
+		List<String> records = readFile("input.txt");
+		Sysqtem.out.println(records[0]);
 	}
+
+	private List<String> readFile(String filename) {
+		List<String> records = new ArrayList<String>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				records.add(line);
+			}
+			reader.close();
+			return records;
+		} catch (Exception e) {
+			System.err.format("Exception occurred trying to read '%s'.", filename);
+			e.printStackTrace();
+			return null;
+		}
 }
 
